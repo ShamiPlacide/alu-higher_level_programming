58d5edbdb7907967b32daa4ff84f5a57e1b1db2c
ShamiPlacide <p.igabe@alustudent.com>
2025-02-28 01:20:28 -0800

    exceptions edited

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
     printed = 0
     for i in range(x):
@@ -18,4 +17,3 @@ def safe_print_list_integers(my_list=[], x=0):
             continue
     print("")
     return printed
-
--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
@@ -24,4 +23,3 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             new_list.append(division)
     return new_list
-
